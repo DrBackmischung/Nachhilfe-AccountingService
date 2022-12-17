febf0be5c65c71f51c29e47c41148875be884436
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-12-17 20:45:42 +0100

    imports

--- src/main/java/de/wi2020sebgroup1/nachhilfe/accounting/entities/Account.java
+++ src/main/java/de/wi2020sebgroup1/nachhilfe/accounting/entities/Account.java
@@ -2,7 +2,6 @@ package de.wi2020sebgroup1.nachhilfe.accounting.entities;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.redis.core.RedisHash;
